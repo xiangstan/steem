19edb148d867daa478b76fde81a9f9598c89ba8d
Xiang Tan <xtan@uccs.edu>
2020-03-22 17:08:11 -0600

    Added hive main template and steem main template

---
+++ js/components/HiveMain.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-half">
+        <searchid :page="page" :steem="steem"></searchid>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-one-third">
+        <prof-box :page="page" :show="show.Profile" :steem="steem" ref="hiveprofile"></prof-box>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports={
+  components:{
+    "prof-box": window.httpVueLoader("./js/components/Profile.vue"),
+    "searchid": window.httpVueLoader("./js/components/SearchId.vue"),
+  },
+  computed: {
+    Lang() { return this.$store.state.lang; },
+    Profile() { return this.$store.state.Profile; },
+    SteemId() { return this.$store.state.SteemId; }
+  },
+  methods: {
+    Init() {
+      if (this.SteemId) {
+        this.$store.commit("setLoading", true);
+        this.searchAccount(this.SteemId);
+      }
+    },
+    searchAccount: function(steemId) {
+      this.$parent.searchAccount(steemId);
+    }
+  },
+  mounted() {
+    this.Init();
+  },
+  props: {
+    page: {type: String, default: "hive"},
+    show: {type: Object, default: false},
+    steem: {type: Object, default: false}
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ js/components/SteemMain.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-half">
+        <searchid :page="page" :steem="steem"></searchid>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-one-third">
+        <prof-box :page="page" :show="show.Profile" :steem="steem" ref="steemprofile"></prof-box>
+        <authorities :apps="Profile.posting.account_auths" ref="authorized" v-if="Profile && Profile.posting"></authorities>
+        <follow :steem="steem"></follow>
+      </div>
+      <div class="column">
+        <!-- <token-list :tokens="tokens.main" v-if="user.main"></token-list> -->
+        <!-- <un-claimed :steem="steem" ref="unclaimed" v-if="user.main"></un-claimed> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports={
+  components:{
+    "authorities": window.httpVueLoader("./js/components/Authorities.vue"),
+    "follow": window.httpVueLoader("./js/components/Follow.vue"),
+    "prof-box": window.httpVueLoader("./js/components/Profile.vue"),
+    "searchid": window.httpVueLoader("./js/components/SearchId.vue"),
+    "token-list": window.httpVueLoader("./js/components/Tokens.vue"),
+    "un-claimed": window.httpVueLoader("./js/components/Unclaimed.vue")
+},
+  computed: {
+    Lang() { return this.$store.state.lang; },
+    Profile() { return this.$store.state.Profile; },
+    SteemId() { return this.$store.state.SteemId; }
+  },
+  methods: {
+    Init() {
+      if (this.SteemId) {
+        this.$store.commit("setLoading", true);
+        this.searchAccount(this.SteemId);
+      }
+    },
+    searchAccount: function(steemId) {
+      this.$parent.searchAccount(steemId);
+    }
+  },
+  mounted() {
+    this.Init();
+  },
+  props: {
+    page: {type: String, default: "steem"},
+    show: {type: Object, default: false},
+    steem: {type: Object, default: false}
+  }
+};
+</script>
+
+<style scoped>
+</style>
